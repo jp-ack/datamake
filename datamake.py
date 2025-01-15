a66e20f4364d9a188d9fdf18d7c1905c14a4eb30
jpmartinez10 <jpierre.s.martinez@gmail.com>
2025-01-15 16:20:48 -0500

    nano sucks , this works

--- datamake.py
+++ datamake.py
@@ -1,6 +1,8 @@
 # This python program will populate a csv with rows and columns 
 import random 
+import csv
 from datetime import datetime
+import sys
 
 
 def makeName():
@@ -29,8 +31,31 @@ def assignOccupation():
     occupations = ["accounting" , "legal" , "human_resources"]
     return random.choice(occupations)
 
-def main():
-    # name , dob , occupation
-    print(makeName().capitalize(),makeDOB(),assignOccupation())
+    
+#generates rows and columns in form of csv    
+def make_csv(num_of_rows,output_file_name):
+    #make the header and csv file
+    header = ["NAME","DOB","JOB"]
+    
+    csv_filename = str(output_file_name)+".csv"
+    
+    with open(csv_filename,mode = "w",newline="") as file:  ## error was happening earlier because the while loop was outside of the 'open' block. which automatically closes the file
+        writer = csv.writer(file)
+        writer.writerow(header)
+       
+        i = 1
+        while (i <= int(num_of_rows)):
+            print(f"Writing row {i}")
+            writer.writerow([makeName(),makeDOB(),assignOccupation()]) ## this issue is that this needs to be a single list 
+            i+=1
+            
+    print(f"finished writing csv ,overwrote {output_file_name}.csv")
+            
 
-main()
+
+#Run    
+def main():
+    make_csv(sys.argv[1],sys.argv[2]) # passes the argument specified on command line 
+
+if __name__ == "__main__":
+    main()
